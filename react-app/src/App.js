5e85082961d022aaaf3fcd1a3bf16a721eb0ee10
Kashish Grover <kashish@hashnode.com>
2017-08-03 17:15:51 +0530

    Understood concepts of stateless functions and classes Understood how comments are written in JSX Understood how JSX is rendered by React

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const App = () => <h1>Hi</h1>
+//Stateless component
+// const App = () => <h1>Hello, Stateless Component</h1>
+
+
+// Class component can have state, but stateless component cannot
+class App extends React.Component{
+  render() {
+    return (
+      <div>
+        {/*This is how you comment inside JSX*/}
+        {/*Return cannot return consecutive tags because it is like
+          returning two consecutive functions which won't translate well.
+          Instead, one is supposed to wrap consecutive tags within a wrapper
+          tag like <div>*/}
+        <h1>Hello World, this is a test</h1>
+        <article>This here is some sample article text.</article>
+      </div>
+    )
+  }
+}
 
 export default App
