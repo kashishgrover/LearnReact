4d5465b38f7aa80f2d5b1dbb9da75c265b2598c7
Kashish Grover <kashish@hashnode.com>
2017-08-04 13:21:09 +0530

    React using refs to access components

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,37 +1,50 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-//React Synthetic Event System
+//React using refs to access components
 
 class App extends React.Component{
-
   constructor() {
     super();
-    this.state = {currentEvent: '---'}
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
   }
 
-  update(e) {
-    this.setState({currentEvent: e.type})
+  update() {
+    this.setState({
+      a: this.a.value,
+      b: this.refs.b.value,
+      c: ReactDOM.findDOMNode(this.c).value
+    })
   }
 
   render() {
     return (
       <div>
-        <textarea
-          onKeyPress={this.update}
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          onFocus={this.update}
-          onBlur={this.update}
-          onMouseOver={this.update}
-          onDoubleClick={this.update}
-          onTouchMove={this.update}
-          cols="30"
-          rows="10" />
-        <h1>{this.state.currentEvent}</h1>
+        {/* Ref can even take a callback */}
+        <input
+          ref={ node => this.a = node }
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.a}
+        <hr />
+        <input
+          ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.b}
+        <hr />
+        <Input
+          ref={component => this.c = component}
+          update={this.update.bind(this)}
+        /> {this.state.c}
       </div>
     )
+  }
+}
+
+class Input extends React.Component {
+  render() {
+    return <input type="text" onChange={this.props.update} />
   }
 }
 
