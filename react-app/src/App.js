effe542798b8da6eb0e901548c152e717370d80b
Kashish Grover <kashish@hashnode.com>
2017-08-04 12:37:09 +0530

    Learnt how to make custom validations in react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
 
+//React Custom propType validation
+
 class App extends React.Component{
   render() {
     return (
       <div>
-        <Button>I <Heart /> React</Button>
+        <Title text="Hello World"/>
+        <Lorem text="This is a test"/>
       </div>
     )
   }
 }
 
-const Button = (props) => <button>{props.children}</button>
-
-class Heart extends React.Component {
+class Title extends React.Component {
   render() {
-    return (
-      <span>
-        &hearts;
-      </span>
-    )
+    return (<h1>Title: {this.props.text}</h1>)
   }
 }
+
+Title.propTypes = {
+  text: React.PropTypes.string.isRequired
+  //This is about the extent of built in validation react can achieve
+}
+
+///////
+const Lorem = (props) => (<h2>Lorem ipsum: {props.text}</h2>)
+
+Lorem.propTypes = {
+  //There is a possibility to create custom validations as follows:
+  text(props, propName, component) {
+    if(!(propName in props)) {
+      return new Error(`missing ${propName}`)
+    }
+    if(props[propName].length < 20) {
+      return new Error(`${propName} should be greater than 20`)
+    }
+  }
+}
+///////
 
 export default App
